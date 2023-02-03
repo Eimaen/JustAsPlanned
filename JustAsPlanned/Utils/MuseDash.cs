927d337e11623e307c60dff7ad8cc38c40a37fce
Yuri <32714790+Eimaen@users.noreply.github.com>
2023-02-03 18:09:33 +0100

    fix Amiya girl not being unlocked

--- JustAsPlanned/Utils/MuseDash.cs
+++ JustAsPlanned/Utils/MuseDash.cs
@@ -13,6 +13,7 @@ namespace JustAsPlanned.Utils
         private static class Patterns
         {
             public static readonly string SteamDlcCheck = @"40 53 48 83 EC ? 8B D9 33 C9 E8 ? ? ? ? 80 3D ? ? ? ? ? 75 ? 8B 0D ? ? ? ? E8 ? ? ? ? C6 05 ? ? ? ? ? 48 8B 05 ? ? ? ? 45 33 C0 8B D3 48 8B 88 ? ? ? ? 48 8B 49 ? 48 83 C4 ? 5B E9 ? ? ? ? CC CC CC CC CC 48 83 EC ? 33 C9 E8 ? ? ? ? 80 3D ? ? ? ? ? 75 ? 8B 0D ? ? ? ? E8 ? ? ? ? C6 05 ? ? ? ? ? 48 8B 05 ? ? ? ? 33 D2 48 8B 88 ? ? ? ? 48 8B 49 ? 48 83 C4 ? E9 ? ? ? ? CC CC CC CC CC CC CC CC CC CC CC CC CC 40 53 48 83 EC ? 8B D9 33 C9 E8 ? ? ? ? 80 3D ? ? ? ? ? 75 ? 8B 0D ? ? ? ? E8 ? ? ? ? C6 05 ? ? ? ? ? 48 8B 05 ? ? ? ? 45 33 C0 8B D3 48 8B 88 ? ? ? ? 48 8B 49 ? 48 83 C4 ? 5B E9 ? ? ? ? CC CC CC CC CC 48 83 EC ? 33 C9 E8 ? ? ? ? 80 3D ? ? ? ? ? 75 ? 8B 0D ? ? ? ? E8 ? ? ? ? C6 05 ? ? ? ? ? 48 8B 05 ? ? ? ? 33 D2 48 8B 88 ? ? ? ? 48 8B 49 ? 48 83 C4 ? E9 ? ? ? ? CC CC CC CC CC CC CC CC CC CC CC CC CC 48 83 EC";
+            public static readonly string SteamDlcPurchaseDate = @"40 53 48 83 EC ? 8B D9 33 C9 E8 ? ? ? ? 80 3D ? ? ? ? ? 75 ? 8B 0D ? ? ? ? E8 ? ? ? ? C6 05 ? ? ? ? ? 48 8B 05 ? ? ? ? 45 33 C0 8B D3 48 8B 88 ? ? ? ? 48 8B 49 ? 48 83 C4 ? 5B E9 ? ? ? ? CC CC CC CC CC 40 55 53";
         }
 
         public static bool Exploit(Process museDash)
@@ -20,11 +21,15 @@ namespace JustAsPlanned.Utils
             try
             {
                 Memory memory = museDash.InitializeMemory();
-                IntPtr psc = memory.PatternScan(Patterns.SteamDlcCheck, memory.GetModuleByName("GameAssembly.dll"));
-                memory.Write(psc, new byte[] { 
-                    0x48, 0xB8, 0x01, 0x00, 0x00, 0x00, 0x00,
-                    0x00, 0x00, 0x00, 
-                    0xC3 
+                IntPtr dlcCheckAddr = memory.PatternScan(Patterns.SteamDlcCheck, memory.GetModuleByName("GameAssembly.dll"));
+                memory.Write(dlcCheckAddr, new byte[] {
+                    0x48, 0xB8, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    0xC3
+                });
+                IntPtr purchaseTimeAddr = memory.PatternScan(Patterns.SteamDlcPurchaseDate, memory.GetModuleByName("GameAssembly.dll"));
+                memory.Write(purchaseTimeAddr, new byte[] {
+                    0xB8, 0xFF, 0xF7, 0x13, 0x37,
+                    0xC3
                 });
             }
             catch { return false; }
