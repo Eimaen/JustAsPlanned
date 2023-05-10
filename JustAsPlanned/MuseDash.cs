7114fb68d9886cb3d6e9a27a942822d991aed373
Yuri <32714790+Eimaen@users.noreply.github.com>
2023-05-10 18:08:53 +0200

    get rid of my memory library

--- JustAsPlanned/MuseDash.cs
+++ JustAsPlanned/MuseDash.cs
@@ -1,10 +1,13 @@
-﻿using MemoryHacking;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace JustAsPlanned
 {
@@ -17,26 +20,118 @@ namespace JustAsPlanned
             public static readonly string IsSelectedAlbumUnlockedMaster = @"48 83 EC ? 80 3D ? ? ? ? ? 75 ? 8B 0D ? ? ? ? E8 ? ? ? ? C6 05 ? ? ? ? ? 48 8B 0D ? ? ? ? F6 81 ? ? ? ? ? 74 ? 83 B9 ? ? ? ? ? 75 ? E8 ? ? ? ? 33 C9 E8 ? ? ? ? 84 C0 0F 85";
         }
 
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] buffer, long size, out long lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In] byte[] buffer, long size, out long lpNumberOfBytesWritten);
+
+        public static byte[] Read(IntPtr handle, IntPtr address, long size)
+        {
+            byte[] buffer = new byte[size];
+            long bytesRead;
+
+            ReadProcessMemory(handle, address, buffer, size, out bytesRead);
+
+            if (bytesRead == 0)
+                return null;
+
+            return buffer;
+        }
+
+        private static IntPtr PatternScan(IntPtr handle, string pattern, IntPtr baseAddress, long scanSize)
+        {
+            List<byte> patternBytes = new List<byte>();
+            List<bool> patternMask = new List<bool>();
+
+            foreach (string byteString in pattern.Split(' '))
+            {
+                if (byteString == "?" || byteString == "00" || byteString == "??")
+                {
+                    patternBytes.Add(0x00);
+                    patternMask.Add(false);
+                }
+                else
+                {
+                    patternBytes.Add(byte.Parse(byteString, NumberStyles.HexNumber));
+                    patternMask.Add(true);
+                }
+            }
+
+            byte[] moduleBytes = Read(handle, baseAddress, scanSize);
+
+            if (moduleBytes == null)
+                return IntPtr.Zero;
+
+            for (int i = 0; i < moduleBytes.Length; i++)
+            {
+                bool found = true;
+
+                for (int l = 0; l < patternMask.Count; l++)
+                {
+                    if (l + i >= moduleBytes.Length) found = false;
+                    else found = !patternMask[l] || moduleBytes[l + i] == patternBytes[l];
+
+                    if (!found) break;
+                }
+
+                if (found)
+                    return new IntPtr(i + baseAddress.ToInt64());
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private static IntPtr PatternScan(Process process, string pattern, string moduleName)
+        {
+            foreach (ProcessModule module in process.Modules)
+                if (module.ModuleName == moduleName)
+                    return PatternScan(process.Handle, pattern, module.BaseAddress, module.ModuleMemorySize);
+            return IntPtr.Zero;
+        }
+
+        public static long Write(IntPtr handle, IntPtr address, byte[] value)
+        {
+            WriteProcessMemory(handle, address, value, value.Length, out long bytesWritten);
+            return bytesWritten;
+        }
+
+        private static bool[] patches = new bool[] { false, false, false };
+
         public static bool Exploit(Process museDash)
         {
             try
             {
-                Memory memory = museDash.InitializeMemory();
-                IntPtr dlcCheckAddr = memory.PatternScan(Patterns.SteamDlcCheck, memory.GetModuleByName("GameAssembly.dll"));
-                memory.Write(dlcCheckAddr, new byte[] {
-                    0x48, 0xB8, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                    0xC3
-                });
-                IntPtr purchaseTimeAddr = memory.PatternScan(Patterns.SteamDlcPurchaseDate, memory.GetModuleByName("GameAssembly.dll"));
-                memory.Write(purchaseTimeAddr, new byte[] {
-                    0xB8, 0x85, 0x47, 0xDE, 0x63,
-                    0xC3
-                });
-                IntPtr isMasterUnlockedAddress = memory.PatternScan(Patterns.IsSelectedAlbumUnlockedMaster, memory.GetModuleByName("GameAssembly.dll"));
-                memory.Write(isMasterUnlockedAddress, new byte[] {
-                    0x48, 0xB8, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                    0xC3
-                });
+                IntPtr dlcCheckAddr = PatternScan(museDash, Patterns.SteamDlcCheck, "GameAssembly.dll");
+                IntPtr purchaseTimeAddr = PatternScan(museDash, Patterns.SteamDlcPurchaseDate, "GameAssembly.dll");
+                IntPtr isMasterUnlockedAddress = PatternScan(museDash, Patterns.IsSelectedAlbumUnlockedMaster, "GameAssembly.dll");
+
+                if (dlcCheckAddr != IntPtr.Zero && purchaseTimeAddr != IntPtr.Zero && isMasterUnlockedAddress != IntPtr.Zero)
+                {
+                    if (!patches[0] && Write(museDash.Handle, dlcCheckAddr, new byte[] {
+                        0x48, 0xB8, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                        0xC3
+                    }) <= 0)
+                        return false;
+                    else
+                        patches[0] = true;
+
+                    if (!patches[1] && Write(museDash.Handle, purchaseTimeAddr, new byte[] {
+                        0xB8, 0x85, 0x47, 0xDE, 0x63,
+                        0xC3
+                    }) <= 0) 
+                        return false;
+                    else
+                        patches[1] = true;
+
+                    if (patches[2] && Write(museDash.Handle, isMasterUnlockedAddress, new byte[] {
+                        0x48, 0xB8, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                        0xC3
+                    }) <= 0) 
+                        return false;
+                    else
+                        patches[2] = true;
+                }
             }
             catch { return false; }
             return true;
